c3b2f3d3b83469105c501a07d34f925531dd01ec
Eric Petsopoulos <epetsopoulos@gmail.com>
2023-10-15 23:00:41 +0100

    add timer

--- src/wav_generator.py
+++ src/wav_generator.py
@@ -2,15 +2,21 @@ import os
 from datetime import datetime
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 import scipy.io.wavfile
+import time
 
 def generate_wav(speed, mood, artists, sound_type):
+    start_time = time.time()  # Record the start time
     processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
-    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-medium")
     
     text_description = f"A {speed} {mood} {sound_type} inspired by {artists}. This audio is designed to be loopable."
     inputs = processor(text=[text_description], padding=True, return_tensors="pt")
     
     audio_values = model.generate(**inputs, max_new_tokens=256)
+
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time  # Calculate elapsed time
+    print(f"Sound generation took {elapsed_time:.2f} seconds.")  # Display the elapsed time
     
     if not os.path.exists('output'):
         os.makedirs('output')
