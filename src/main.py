037bc201444994d0c1dca1d0c904c6bce879a1f8
Eric Petsopoulos <epetsopoulos@gmail.com>
2023-10-16 18:55:22 +0100

    refactor main.py

--- src/main.py
+++ src/main.py
@@ -13,56 +13,57 @@ class MainApp:
         self.cli = CLIHelpers()
         self.wav_gen = None
 
-    def run(self):
+    def setup(self):
         warnings.filterwarnings('ignore', message='torch.nn.utils.weight_norm is deprecated')
-
         self.cli.display_logo()
         self.cli.print_green("Welcome to the AI WAV Generator!")
 
+    def get_user_decision(self):
+        user_input = self.cli.input_grey("Do you want to generate another sample? (yes/no): ").strip().lower()
+        return user_input == 'yes'
+
+    def generate_and_play_audio(self):
+        self.wav_gen.generate()
+        self.wav_generation_done_flag.set()
+        self.cli.print_green("\nPreviewing the generated audio... 🔊")
+        sd.play(self.wav_gen.generated_audio_data, samplerate=self.wav_gen.generated_sampling_rate)
+        sd.wait()
+
+    def save_generated_audio(self):
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        sample_name = f"{self.wav_gen.musical_key}_{self.wav_gen.bpm}BPM_{self.wav_gen.length}_{self.wav_gen.mood}_{self.wav_gen.sound_type}_{timestamp}.wav"
+        output_path = os.path.join('output', sample_name)
+        scipy.io.wavfile.write(output_path, rate=self.wav_gen.generated_sampling_rate, data=self.wav_gen.generated_audio_data)
+        self.cli.print_green(f"Saved in './{output_path}' 💾")
+
+    def run(self):
+        self.setup()
         running = True
-
         while running:
-            self.wav_generation_done_flag.clear()
-            length, quality, bpm, musical_key, mood, artists, sound_type = self.cli.get_user_input()
-
-            proceed = self.cli.confirm_and_generate(length, quality, bpm, musical_key, mood, artists, sound_type)
+            self.wav_generation_done_flag.clear()  # Reset the flag for the new iteration
+            user_params = self.cli.get_user_input()
+            proceed = self.cli.confirm_and_generate(*user_params)
             
             if not proceed:
-                user_input = self.cli.input_grey("Do you want to generate another sample? (yes/no): ").strip().lower()
-                if user_input != 'yes':
-                    running = False
-                    break
-                else:
-                    continue
+                running = self.get_user_decision()
+                continue
 
-            self.wav_gen = WAVGenerator(length, quality, bpm, musical_key, mood, artists, sound_type)
-            pattern_timer_thread = threading.Thread(target=self.cli.display_animated_wave_with_timer, args=(self.wav_generation_done_flag,), daemon=True)
-            pattern_timer_thread.start()
+            self.wav_gen = WAVGenerator(*user_params)
+
+            wave_thread = threading.Thread(target=self.cli.display_animated_wave_with_timer, args=(self.wav_generation_done_flag,))
+            wave_thread.start()
 
             try:
-                self.wav_gen.generate()
+                self.generate_and_play_audio()
                 self.wav_generation_done_flag.set()
-
-                self.cli.print_green("\nPreviewing the generated audio... 🔊")
-                sd.play(self.wav_gen.generated_audio_data, samplerate=self.wav_gen.generated_sampling_rate)
-                sd.wait()
-
                 save_audio = self.cli.input_grey("Do you want to save this audio? (yes/no): ").strip().lower()
                 if save_audio == 'yes':
-                    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-                    sample_name = f"{self.wav_gen.musical_key}_{self.wav_gen.bpm}BPM_{self.wav_gen.length}_{self.wav_gen.mood}_{self.wav_gen.sound_type}_{timestamp}.wav"
-                    output_path = os.path.join('output', sample_name)
-                    scipy.io.wavfile.write(output_path, rate=self.wav_gen.generated_sampling_rate, data=self.wav_gen.generated_audio_data)
-                    self.cli.print_green(f"Saved in './{output_path}' 💾")
-
+                    self.save_generated_audio()
+                
+                running = self.get_user_decision()
             except Exception as e:
                 self.cli.print_red(f"An error occurred: {e}")
-
-            pattern_timer_thread.join()
-
-            user_input = self.cli.input_grey("Do you want to generate another sample? (yes/no): ").strip().lower()
-            if user_input != 'yes':
-                running = False
+                self.wav_generation_done_flag.set()
 
         self.cli.print_green("Exiting the program. Goodbye! 👋")
 
